90c1830f47756490c10de9068a74a232b2151beb
h-miiaah <haim.miiaah5@gmail.com>
2021-12-30 21:15:16 -0500

    final return added

--- Implementstrstr.js
+++ Implementstrstr.js
@@ -8,6 +8,9 @@
     if (needle === haystack) return 0;
 
     for (let i = 0; i <= haystack.length - needle.length; i++) {
-        
+        if (needle === haystack.substring(i, i + needle.length)) {
+            return i;
+        }
     }
+    return -1;
 };
